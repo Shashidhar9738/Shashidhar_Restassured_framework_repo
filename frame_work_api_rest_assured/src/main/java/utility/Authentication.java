122d1d60c19a978bd53dbd3f39ca2262cc9256b8
Shashidhar9738 <137638115+Shashidhar9738@users.noreply.github.com>
2023-07-18 23:24:05 +0530

    rest assured framework

--- frame_work_api_rest_assured/src/main/java/utility/Authentication.java
+++ frame_work_api_rest_assured/src/main/java/utility/Authentication.java
@@ -2,7 +2,7 @@ package utility;
 
 public class Authentication {
 	
-	public final static String bearer_token="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";
+	public final static String bearer_token="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";
 	
 	public static String getbearertoken() {
 		return bearer_token;
